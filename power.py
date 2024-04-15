619a015104a99b153fa55d10a167f3158c71fa56
Florian PASTEUR <florian.c.p@pm.me>
2024-04-15 23:02:36 +0200

    clear logging

--- power.py
+++ power.py
@@ -18,7 +18,7 @@ from openant.devices.fitness_equipment import (
 def main():
     import logging
 
-    logging.basicConfig(level=logging.INFO)
+    # logging.basicConfig(level=logging.INFO)
     node = Node()
     node.set_network_key(0x00, ANTPLUS_NETWORK_KEY)
     devices = []
